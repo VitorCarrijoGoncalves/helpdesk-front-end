f661fb4ade0863d1edf57c35ba74b8cbccfb470d
raonedaniel <vitorcg002@gmail.com>
2019-09-25 19:43:20 -0300

    component user-new and response model of typescript class implementated

--- src/app/components/user-new/user-new.component.ts
+++ src/app/components/user-new/user-new.component.ts
@@ -1,3 +1,4 @@
+import { ResponseApi } from './../../model/response-api';
 import { UserService } from './../../services/user.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
@@ -28,10 +29,66 @@ export class UserNewComponent implements OnInit {
    }
 
   ngOnInit() {
+    let id: string = this.route.snapshot.params['id'];
+    if (id !== undefined) {
+      this.findById(id);
+    }
   }
 
   findById(id: string) {
-    this.userService.findById(id).subscribe((responseAPI: ResponseApi))
+    this.userService.findById(id).subscribe((responseAPI: ResponseApi) => {
+      this.user = responseAPI.data;
+      this.user.password = '';
+    }, err => {
+      this.showMessage({
+        type: 'error',
+        text: err['error']['errors'][0]
+      });
+    });
+  }
+
+  register() {
+    this.message = {};
+    this.userService.createOrUpdate(this.user).subscribe((responseApi: ResponseApi) => {
+      this.user = new User('', '', '', '');
+      const userRet: User = responseApi.data; // let
+      this.form.resetForm();
+      this.showMessage({
+        type: 'success',
+        text: `Registered ${userRet.email}`
+      });
+    }, err => {
+
+      this.showMessage({
+        type: 'error',
+        text: err['error']['errors'][0]
+      });
+
+    });
+  }
+
+  private showMessage(message: {type: string, text: string}): void {
+    this.message = message;
+    this.buildClasses(message.type);
+    setTimeout(() => {
+      this.message = undefined;
+    }, 3000);
+  }
+
+  private buildClasses(type: string): void {
+    this.classCss = {
+      'alert': true
+    }
+
+    this.classCss['alert-' + type] = true;
+  }
+
+  getFromGroupClass(isInvalid: boolean, isDirty): {} {
+    return {
+      'form-group': true,
+      'has-error': isInvalid && isDirty,
+      'has-success': !isInvalid && isDirty
+    };
   }
 
 }
